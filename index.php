ad4028280d8671fb81c126f45eeab4bc1136c29b
Lee-Martin Clarke <mobeel5356@gmail.com>
2018-02-22 13:41:24 -0500

    init2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+
+<head>
+    <Title>E-Library</Title>
+    <!-- JavaScript INCOMING -->
+    <script type="text/javascript" src=""></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
+
+    <!-- CSS INCOMING -->
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <p>This is the swag!!!</p>
+
+</body>
+
+</html>
